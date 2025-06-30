c9fc7b3bbaf94d7fd8aad0e748dcceabf0826aea
veluaru <angie.castaneda@guane.com.co>
2025-06-30 00:02:04 -0500

    :lipstick: build config

--- dist/static/chunks/src_app_details_page_tsx_19c88122._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_08aebe19._.js",
-  "static/chunks/src_c0ce27b5._.js"
-],
-    source: "dynamic"
-});
--- dist/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__bb03853a._.css",
-  "static/chunks/_256d198b._.js"
-],
-    source: "dynamic"
-});
