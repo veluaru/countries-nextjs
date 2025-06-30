443897697a6ef2e192a42cc6ee9686ceef609cea
Angie Castañeda <42279901+veluaru@users.noreply.github.com>
2025-06-29 23:33:27 -0500

    Delete dist directory

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i6/WbElVxuo8tYha6baOWFR7l6nuphYqMypuYOSZNuM=\"\n}"
--- dist/static/chunks/src_app_page_tsx_19c88122._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_918cd437._.js",
-  "static/chunks/src_3f1e742e._.js"
-],
-    source: "dynamic"
-});
